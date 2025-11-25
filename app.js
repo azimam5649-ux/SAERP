b12359570b8051a0e6815aa65ca14ee1d5616aa1
azimam5649-ux <azimam5649@gmail.com>
2025-11-25 22:14:55 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ if (HOST === '172.30.1.42' || HOST === 'localhost' || HOST === '127.0.0.1') {
   // 2. GitHub Pages / 외부에서 접속할 때 (https 사용)
   // 🚨 이 부분을 찾아 'http'를 'https'로 변경해야 합니다.
   // 443 포트를 포워딩했으므로 포트 번호는 생략합니다.
-  API_BASE = 'https://saerp.synology.me/saerp/api'; 
+  API_BASE = 'https://api.saerp.synology.me';
 }
 
 const ADMIN_ID = 'admin', ADMIN_PW = '1234';
